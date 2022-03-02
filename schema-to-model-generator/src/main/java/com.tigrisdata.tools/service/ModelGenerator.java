48104cfe6104db1f702251b9724ae539e76400a5
Jigar Joshi <jigarjm@gmail.com>
2022-03-01 16:27:58 -0800

    minor beautification

--- schema-to-model-generator/src/main/java/com.tigrisdata.tools/service/ModelGenerator.java
+++ schema-to-model-generator/src/main/java/com.tigrisdata.tools/service/ModelGenerator.java
@@ -5,7 +5,10 @@ import com.tigrisdata.tools.config.JavaCodeGenerationConfig;
 import java.io.File;
 import java.util.List;
 
+/**
+ * Generates the Model from schema files
+ */
 public interface ModelGenerator {
-  void generate(List<File> schemaFiles, JavaCodeGenerationConfig options)
-      throws IllegalStateException;
+    void generate(List<File> schemaFiles, JavaCodeGenerationConfig options)
+            throws IllegalStateException;
 }

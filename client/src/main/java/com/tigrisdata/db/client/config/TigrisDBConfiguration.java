de520bded5110a28811bdf500069be4404c4ff6c
Jigar Joshi <jigarjm@gmail.com>
2022-03-29 15:03:15 -0700

    Opensource preparation

--- client/src/main/java/com/tigrisdata/db/client/config/TigrisDBConfiguration.java
+++ client/src/main/java/com/tigrisdata/db/client/config/TigrisDBConfiguration.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2022 Tigris Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tigrisdata.db.client.config;
 
 import java.time.Duration;
@@ -6,13 +19,14 @@ public class TigrisDBConfiguration {
   private final String baseURL;
   private final TigrisDBConfiguration.NetworkConfig network;
 
-  public TigrisDBConfiguration(TigrisDBConfiguration.TigrisDBClientConfigurationBuilder builder) {
+  private TigrisDBConfiguration(TigrisDBConfiguration.TigrisDBClientConfigurationBuilder builder) {
     this.baseURL = builder.baseURL;
     this.network = builder.network;
   }
 
-  public static TigrisDBConfiguration.TigrisDBClientConfigurationBuilder newBuilder() {
-    return new TigrisDBConfiguration.TigrisDBClientConfigurationBuilder();
+  public static TigrisDBConfiguration.TigrisDBClientConfigurationBuilder newBuilder(
+      final String baseURL) {
+    return new TigrisDBConfiguration.TigrisDBClientConfigurationBuilder(baseURL);
   }
 
   public String getBaseURL() {
@@ -25,18 +39,12 @@ public class TigrisDBConfiguration {
 
   public static final class TigrisDBClientConfigurationBuilder {
 
-    private String baseURL;
+    private final String baseURL;
     private TigrisDBConfiguration.NetworkConfig network;
-    public static final String DEFAULT_BASE_URL = "https://dev.tigrisdata.cloud/api/";
 
-    private TigrisDBClientConfigurationBuilder() {
-      this.baseURL = DEFAULT_BASE_URL;
-      this.network = NetworkConfig.newBuilder().build();
-    }
-
-    public TigrisDBConfiguration.TigrisDBClientConfigurationBuilder withBaseURL(String baseURL) {
+    private TigrisDBClientConfigurationBuilder(String baseURL) {
       this.baseURL = baseURL;
-      return this;
+      this.network = NetworkConfig.newBuilder().build();
     }
 
     public TigrisDBConfiguration.TigrisDBClientConfigurationBuilder withNetwork(
